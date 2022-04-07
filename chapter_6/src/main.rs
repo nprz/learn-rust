3da50e25d9bc6098ac12275b724368b9d8de60a0
Nick <przebienda.n@gmail.com>
2022-04-06 22:11:30 -0400

    6.1

---
+++ chapter_6/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    /* 7pm - complete chapter 6 by 9pm */
+
+    // Chapter 6 - Enums and Patter Matching
+
+    // Enums allow you to define a type by enumerating its possible variants
+    // Chapter objectives:
+    // define and show how an enum can encode meaning along with data
+    // explore the enum Option, which expresses a value can be either something or nothing
+    // look at pattern matching with the match expression and how it makes it easy to run different code for different values of an enum
+    // cover how the if let construct is another convenient and concise idiom avialable to handle enums
+
+}   
